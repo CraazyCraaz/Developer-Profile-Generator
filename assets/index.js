55f1901b58be9457e432b5a896240b03787263bb
Jenny <j.thurnau@hotmail.com>
2020-01-09 19:04:12 -0800

    html

--- assets/index.js
+++ assets/index.js
@@ -5,57 +5,57 @@ const generateHTML = require("./generateHTML");
 
 function getUserInput() {
   return inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "Enter the GitHub username of the Developer you're searching for:",
-      name: "username"
-    },
-    {
-      type: "list",
-      message: "Pick a color",
-      name: "color",
-      choices: [
-        "green",
-        "blue",
-        "pink",
-        "red"
-      ]
-    }
-  ])
+    .prompt([
+      {
+        type: "input",
+        message: "Enter the GitHub username of the Developer you're searching for:",
+        name: "username"
+      },
+      {
+        type: "list",
+        message: "Pick a color",
+        name: "color",
+        choices: [
+          "green",
+          "blue",
+          "pink",
+          "red"
+        ]
+      }
+    ])
 };
 
 function init() {
   // this is where the inquirer answers are recorded
   getUserInput().then(userInput => {
     // only sending the username into the getGtiHubUserInfo function
-    getGitHubUserInfo(userInput.username,userInput.color)
+    getGitHubUserInfo(userInput.username, userInput.color)
 
   })
 };
 
-function getGitHubUserInfo(username,color) {
+function getGitHubUserInfo(username, color) {
   const queryUrl = `https://api.github.com/users/${username}`;
-  
+
   axios.get(queryUrl).then(result => {
 
-     var data={
-       apiResults: result,
-       color: color
-     }
-   
-    console.log( generateHTML(data));
+    var data = {
+      apiResults: result,
+      color: color
+    }
+
+    console.log(generateHTML(data));
 
     let userData = generateHTML(data)
 
-    fs.writeFile("devProfile.html", userData, function(err) {
-      if(err) {
+    fs.writeFile("devProfile.html", userData, function (err) {
+      if (err) {
         console.log(err);
       }
       console.log("Success!");
-      
+
     })
-    
+
   })
 };
 
@@ -102,7 +102,7 @@ function writeToFile(fileName, data) {
     if (err) throw err;
     console.log('Success!');
   });
-  
+
 };
 
 
